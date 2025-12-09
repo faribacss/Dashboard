3d625e7bb6d19cae35303432e4b84c7e4dd91f84
Fariba Shahsavan <f.shahsavanpour@gmail.com>
2025-12-09 13:18:49 +0330

    Configure GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import path from "path";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "",
+  base: "/Dashboard/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
